27241e4cefdabdaa2d2e1035d9fee09bc650ce64
Hanru Xu <xjhzwx@gmail.com>
2022-09-18 20:30:08 +1000

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'daf9890958db4d8846c2a41116416172'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd6254155-98a3-40ca-9fc9-39f76f04eeca'
+};
